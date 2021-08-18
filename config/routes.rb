9c1e733238b482dda569a3242f5fbaa293ac64b2
Corey Plotkin <corey.plotkin@gmail.com>
2021-08-18 16:45:07 -0500

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+    resources :restaurants do
+      resources :reviews, only: [ :new, :create, :index ]
+    end
+    resources :reviews, only: [ :show ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "111-111-1111", category: "french" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "222-111-1111", category: "italian" }
+dominos = { name: "Dishoom", address: "7769 Mecuco St, Roma E2 7JE", phone_number: "333-111-1111", category: "japanese" }
+taco_bell = { name: "Pizza East", address: "55A Lion Low St, Rio E1 6PQ", phone_number: "444-111-1111", category: "chinese" }
+mcdonalds = { name: "McDonalds", address: "87 Gofnf'aj St, SF E5 76", phone_number: "555-111-1111", category: "belgian" }
+
+[ dishoom, pizza_east, dominos, taco_bell, mcdonalds ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
